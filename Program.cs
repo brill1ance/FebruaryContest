b0d1f958b0be0e068c0c60d67f56fb520a017f35
brill1ance <gerritkleinhans1@gmail.com>
2020-02-12 15:23:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FebruaryContestEntry
+{
+	class Program
+	{
+		const char block = '▓';
+
+		static void Main(string[] args)
+		{
+			// The solution implementation goes here
+			
+			Console.ForegroundColor = ConsoleColor.Green;
+		
+			//Console.WriteLine(new string(block, 30));
+
+			for (int i= 0; i < 15; i++)
+			{
+				
+				for (int x = 0; x < 30; x++)
+				{
+					if (i > 0 && i < 14)
+						Console.ForegroundColor = (x > 0 && x < 29 ? ConsoleColor.Yellow : ConsoleColor.Green);
+					Console.Write(block);
+				}
+				Console.WriteLine();
+			}
+
+			//Console.WriteLine(new string(block, 30));
+
+			Console.ResetColor();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Yay, a colorful shape!");
+			Console.ReadLine();
+		}
+	}
+}
